300edacf51cba98a9a0fe514e65ddf72468fe483
Sam Zagrobelny <strangesast@gmail.com>
2020-10-02 15:22:28 -0400

    install both x64 and x86 on x64

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
+libinstall()
+{
+  cp "$base/libfwlib32-$platform-$arch.so.$version" "$libdir/libfwlib32.so.$version"
+  ln -sf "$libdir/libfwlib32.so.$version" "$libdir/libfwlib32.so"
+}
+
 version=1.0.5
 platform=linux
 arch=$(arch)
 base="$(dirname "$(realpath "$0")")"
+libdir=/lib
 if [ "$arch" = "x86_64" ] || [ "$arch" = "x86" ]; then
   arch=x86
+  libdir=/lib
+  libinstall
+  arch=x64
+  libdir=/lib64
+  libinstall
 elif [ "$arch" = "armv7l" ]; then
   arch=armv7
+  libdir=/lib
+  libinstall
 else
   echo "unsupported arch"
   exit 1
 fi
 cp fwlib32.h /usr/include/
-cp "$base/libfwlib32-$platform-$arch.so.$version" "/usr/local/lib/libfwlib32.so.$version"
-ln -sf /usr/local/lib/libfwlib32.so.$version /usr/local/lib/libfwlib32.so
 ldconfig
