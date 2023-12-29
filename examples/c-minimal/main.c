2d1153bfe92b8e63b1d30231ab8f7c56b8f4ff7d
Sam Zagrobelny <strangesast@gmail.com>
2023-12-29 11:43:47 -0500

    modified example

--- examples/c-minimal/main.c
+++ examples/c-minimal/main.c
@@ -7,6 +7,31 @@
 #define MACHINE_HOST "127.0.0.1"
 #define MACHINE_PORT 8193
 
+typedef struct odbdy2_t {
+    int16_t  dummy ;     /* not used                */
+    int16_t  axis ;      /* axis number             */
+    int32_t   alarm ;     /* alarm status            */
+    int32_t   prgnum ;    /* current program number  */
+    int32_t   prgmnum ;   /* main program number     */
+    int32_t   seqnum ;    /* current sequence number */
+    int32_t   actf ;      /* actual feedrate         */
+    int32_t   acts ;      /* actual spindle speed    */
+    union {
+        struct {
+            int32_t  absolute[MAX_AXIS] ; /* absolute */
+            int32_t  machine[MAX_AXIS] ;  /* machine  */
+            int32_t  relative[MAX_AXIS] ; /* relative */
+            int32_t  distance[MAX_AXIS] ; /* distance to go */
+        } faxis ; /* In case of all axes */
+        struct {
+            int32_t  absolute ; /* absolute */
+            int32_t  machine ;  /* machine  */
+            int32_t  relative ; /* relative */
+            int32_t  distance ; /* distance to go */
+        } oaxis ; /* In case of 1 axis */
+    } pos ;
+} ODBDY2_T ;
+
 int main(int argc, char *argv[]) {
   int allocated = 0;
   int ret = 0;
@@ -37,6 +62,18 @@ int main(int argc, char *argv[]) {
 
   snprintf(cnc_id, 40, "%08x-%08x-%08x-%08x", cnc_ids[0], cnc_ids[1],
            cnc_ids[2], cnc_ids[3]);
+  printf("machine id: %s\n", cnc_id);
+
+  ODBDY2_T buf;
+  if (cnc_rddynamic2(libh, -1, sizeof(buf), (ODBDY2 *) &buf) != EW_OK) {
+    fprintf(stderr, "Failed to read cnc dynamic info!\n");
+    ret = 1;
+    goto cleanup;
+  }
+
+  printf("Current program = %d\n", buf.prgnum);
+
+  printf("Spindle speed = %d\n", buf.acts);
 
 cleanup:
   if (allocated && cnc_freelibhndl(libh) != EW_OK)
@@ -45,5 +82,4 @@ cleanup:
   cnc_exitprocess();
 #endif
 
-  printf("machine id: %s\n", cnc_id);
 }
